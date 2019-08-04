536f556d85b6c4837f31e522dfe12362c66d3bcc
xingdu <lizongxingsir@163.com>
2019-08-05 00:17:16 +0800

    use cdn

--- includes/helpers/page.js
+++ includes/helpers/page.js
@@ -91,6 +91,6 @@ module.exports = function (hexo) {
 
     hexo.extend.helper.register('get_thumbnail', function (post) {
         const hasThumbnail = hexo.extend.helper.get('has_thumbnail').bind(this)(post);
-        return this.url_for(hasThumbnail ? post.thumbnail : 'images/thumbnail.svg');
+        return this.url_for(hasThumbnail ? post.thumbnail : 'http://cdn.lzx0906.cn/images/thumbnail.svg');
     });
 }
